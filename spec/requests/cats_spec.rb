3e6c94072698065603b8c902d0b128c70f4fedc0
ajgahan19 <andregahan77@gmail.com>
2022-12-15 13:26:54 -0800

    endpoints update and delete strech finished

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -17,5 +17,55 @@ require 'rails_helper'
         expect(response).to have_http_status(200)
         expect(cat.length).to eq 1
       end
+    end 
+    describe "POST /create" do
+      it "creates a cat" do
+        # The params we are going to send with the request
+        cat_params = {
+          cat: {
+            name: 'Buster',
+            age: 4,
+            enjoys: 'Meow Mix, and plenty of sunshine.',
+            image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+          }
+        }
+    
+        # Send the request to the server
+        post '/cats', params: cat_params
+    
+        # Assure that we get a success back
+        expect(response).to have_http_status(200)
+    
+        # Look up the cat we expect to be created in the db
+        cat = Cat.first
+    
+        # Assure that the created cat has the correct attributes
+        expect(cat.name).to eq 'Buster'
+      end
+      describe "PATCH /update" do
+      it "update a cat" do
+        Cat.create(
+          name: 'Felix',
+          age: 2,
+          enjoys: 'Walks in the park',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        )
+        cat_params = {
+          cat: {
+          name: 'Buster',
+          age: 4,
+          enjoys: 'Meow Mix, and plenty of sunshine.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      cat = Cat.last 
+      patch "/cats/#{cat.id}", params: cat_params 
+        
+
+
+
+        patch '/cats', params: cat_params
+        expect(response).to have_http_status(200)
+      end
     end
   end
