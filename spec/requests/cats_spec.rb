be163b64f6c929967cc92da501f0432db571fdb0
TKash89 <tyler.kasheta@outlook.com>
2022-12-16 11:40:09 -0800

    Validations complete

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -10,17 +10,17 @@ require 'rails_helper'
           image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
         )
   
-        # Make a request
+        
         get '/cats'
   
         cat = JSON.parse(response.body)
         expect(response).to have_http_status(200)
         expect(cat.length).to eq 1
       end
-    end 
+    end
+  
     describe "POST /create" do
       it "creates a cat" do
-        # The params we are going to send with the request
         cat_params = {
           cat: {
             name: 'Buster',
@@ -30,20 +30,93 @@ require 'rails_helper'
           }
         }
     
-        # Send the request to the server
+        
         post '/cats', params: cat_params
-    
-        # Assure that we get a success back
         expect(response).to have_http_status(200)
-    
-        # Look up the cat we expect to be created in the db
         cat = Cat.first
-    
-        # Assure that the created cat has the correct attributes
         expect(cat.name).to eq 'Buster'
       end
-      describe "PATCH /update" do
-      it "update a cat" do
+    
+
+
+
+
+
+
+
+
+
+      it "doesn't create a cat without a name" do
+        cat_params = {
+          cat: {
+            age: 2,
+            enjoys: 'Walks in the park',
+            image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+          }
+        }
+    
+        post '/cats', params: cat_params
+        expect(response.status).to eq 422
+        json = JSON.parse(response.body)
+        expect(json['name']).to include "can't be blank"
+      end
+
+      it "doesn't create a cat without a age" do
+        cat_params = {
+          cat: {
+            name: 'Felix',
+            
+            enjoys: 'Walks in the park',
+            image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+          }
+        }
+    
+        post '/cats', params: cat_params
+        expect(response.status).to eq 422
+        json = JSON.parse(response.body)
+        expect(json['age']).to include "can't be blank"
+
+    end
+
+    it "doesn't create a cat without enjoys" do
+      cat_params = {
+        cat: {
+          name: 'Felix',
+          age: 2,
+          
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+  
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['enjoys']).to include "can't be blank"
+
+    end  
+    
+    it "doesn't create a cat without an image" do
+      cat_params = {
+        cat: {
+          name: 'Felix',
+          age: 2,
+          enjoys: 'Walks in the park',
+          
+        }
+      }
+  
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['image']).to include "can't be blank"
+
+    end  
+  end
+    
+    
+    
+    describe "PATCH /update" do
+      it "updates a cat" do
         Cat.create(
           name: 'Felix',
           age: 2,
@@ -60,12 +133,10 @@ require 'rails_helper'
       }
       cat = Cat.last 
       patch "/cats/#{cat.id}", params: cat_params 
-        
-
-
-
-        patch '/cats', params: cat_params
-        expect(response).to have_http_status(200)
+      expect(response).to have_http_status(200)
+      cat=Cat.last
+      expect(cat.name).to eq("Buster")
       end
     end
   end
+  
